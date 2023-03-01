396563b697ccd35932e0edf5d26266998551cd99
Fernando Baladei <f.baladei@gmail.com>
2023-03-01 06:26:03 -0300

    work at treatments

--- src/components/Treatments.jsx
+++ src/components/Treatments.jsx
@@ -7,22 +7,28 @@ export const Treatments = () => {
 
         <div className="bodily">
             <img src="https://luizadermatofuncional.com.br/wp-content/uploads/2018/07/Ultrassom-Interna.jpg" alt="" />
+            <div className="text-bodily">
+            <h2>Corporais</h2>
             <p>
                 Os tratamentos corporais são realizados através de equipamentos de alta tecnologia, para deixar o corpo em harmonia. Eles visam     melhorar    as curvas do corpo, a aparência das estrias, diminuir a celulite, diminuir a gordura localizada, melhorar a flacidez,   aumentando a     produção de colágeno e elastina, melhorar o fluxo sanguíneo e linfático e a aparência da pele corporal.
-            </p><br />
-            <button>Veja Mais</button>
+            </p>
+            </div>
+           
         </div>
+        <button>Veja Mais</button>
 
         <div className="facials">
-            <img src="https://unicathedral.edu.br/vestibulares/wp-content/uploads/sites/30/2021/12/8-768x614.png" alt="" />
+            <div className="text-facials">
+            <h2>Faciais</h2>
             <p>
                 Os tratamentos faciais são indicados para flacidez, rugas, linhas de expressão, manchas, acne, poros dilatados, olheiras, entre     outros. Aqui na clínica trabalhamos com diversos tipos de equipamentos, para prevenir o envelhecimento cutâneo. Através da avaliação    facial da pele e consulta será indicado o tipo de tratamento e quantas sessões serão necessárias para um resultado satisfatório.
-            </p><br />
-            <button>Veja Mais</button>
+            </p>
+            </div>
+          
+            <img src="https://unicathedral.edu.br/vestibulares/wp-content/uploads/sites/30/2021/12/8-768x614.png" alt="" />
         </div>
+        <button>Veja Mais</button>
 
-
-        
     </div>
   )
 }
