ae4a624763f3cb15ca767f9d2ae511ea64b68278
Fernando Baladei <f.baladei@gmail.com>
2023-03-04 20:40:04 -0300

    starting components e changing styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Contact } from './components/Contact'
 import { Main } from './components/Main'
 import { Sidebar } from './components/Sidebar'
 import { Treatments } from './components/Treatments'
+import { Doubt } from './components/Doubt'
+import { About } from './components/About'
 
 function App() {
 
@@ -10,7 +12,9 @@ function App() {
     <div className="App">
       <Sidebar />
       <Main />
+      <About />
       <Treatments />
+      <Doubt />
       <Contact />
     </div>
   )
---
+++ src/components/Doubt.jsx
@@ -0,0 +1,7 @@
+import "./styles/doubt.css"
+
+export const Doubt = () => {
+  return (
+    <div>Doubt</div>
+  )
+}
