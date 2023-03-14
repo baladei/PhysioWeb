4a4fbeb30f6e95e831f26322db359aea0e134601
Fernando Baladei <f.baladei@gmail.com>
2023-03-14 05:41:22 -0300

    ending Doubt styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
         <h2>Karina Letícia de Carvalho</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet dolore minus tempora, ex accusantium saepe eligendi amet voluptates veritatis quod repudiandae corrupti illum incidunt neque et perferendis nesciunt assumenda ab. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis enim dolore, consectetur sunt quae impedit! Quis natus maxime saepe cupiditate error consectetur accusamus earum, magni nostrum repellendus similique dolor ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis labore consequatur aut odio deleniti adipisci alias qui? Doloribus voluptatem ab eum eius, itaque tenetur, officiis ea dicta tempore nihil quibusdam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, ea facilis. Aspernatur praesentium, nam suscipit harum quidem totam expedita impedit sapiente fugiat tempore omnis, repudiandae, modi porro incidunt rerum reprehenderit.</p>
         <img src="https://scontent.fcgh6-1.fna.fbcdn.net/v/t39.30808-6/324182385_1547640012328910_3644212686212593141_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGecGjRJIgO2rNQQmrRTGk_OfECZuTxfJI58QJm5PF8klQtQWgSBQzEISXroof7tzRNyPBLbzbn9KWa4ywzkAz5&_nc_ohc=gUXwJnlx2IkAX_osOMo&_nc_ht=scontent.fcgh6-1.fna&oh=00_AfAQE5M8nl4badqFwIPC8QCRLxPLbKtRlF-4pFxqJgj2mg&oe=6404306A" alt="Karina Letícia" />
-        <h3>Fisioterapeuta Dermatofuncional</h3>     
+        <span>Fisioterapeuta Dermatofuncional</span>     
     </div>
   )
 }
