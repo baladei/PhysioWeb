1802f0c01d4e37fa9c9009cb97a5d57073dbf03c
Fernando Baladei <f.baladei@gmail.com>
2023-03-15 06:38:55 -0300

    almost done Contact styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,29 @@ import "./styles/contact.css"
 
 export const Contact = () => {
   return (
+
     <div className="contact">
-        Contato 
+
+        <form action="" method="post">
+
+          <fieldset className="form">
+
+            <legend>Fale conosco</legend>
+
+            <label for="name">Nome</label>
+            <input type="text" id="name" name="name" placeholder="Digite seu nome completo" autoFocus/>
+
+            <label for="email">E-mail</label>
+            <input type="text" id="email" name="email" placeholder="exemplo@exemplo.com.br" />
+
+            <label for="message">Mensagem</label>
+            <textarea id="message" name="message" placeholder="Digite aqui a sua mensagem" cols="30" rows="10"></textarea>
+
+            <button type="submit">Enviar</button>
+
+          </fieldset>
+
+        </form> 
     </div>
   )
 }
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,8 +2,8 @@ import { BsInstagram, BsFacebook, BsTwitter } from "react-icons/bs"
 import "./styles/social.css"
 
 const socialNetworks = [
-    { name: "instagram", icon:  <BsInstagram />, link: "https://www.instagram.com/fernandobaladei/"},
-    { name: "facebook", icon: <BsFacebook />, link: "https://pt-br.facebook.com/fernando.baladei/"},
+    { name: "instagram", icon:  <BsInstagram />, link: "https://www.instagram.com/karinalecarvalho/"},
+    { name: "facebook", icon: <BsFacebook />, link: "https://www.facebook.com/karina.carvalho.90813"},
     { name: "twitter", icon: <BsTwitter />, link: "https://google.com"},
 ]
 
